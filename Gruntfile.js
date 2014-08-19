b1f8bb78a3dcadb43aa494cd5069d26cec858473
Andrew Nichols <tandrewnichols@gmail.com>
2014-08-19 11:07:12 -0400

    Update to use new task-master

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 var tm = require('task-master');
 
 module.exports = function(grunt) {
-  tm(grunt);
+  tm(grunt, { production: true, development: false });
   grunt.registerTask('default', ['clean', 'symlink', 'concat:dev', 'less:dev', 'markdown', 'server', 'watch']);
-  grunt.registerTask('heroku:production', ['clean', 'symlink', 'concat:dist', 'uglify:dist', 'less:dist', 'markdown']);
+  grunt.registerTask('build', ['clean', 'symlink', 'concat:dist', 'uglify:dist', 'less:dist', 'markdown']);
 };

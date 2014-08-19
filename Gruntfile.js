5b885ad67456425bfe3fd0c8b430501a2fdfa128
Andrew Nichols <tandrewnichols@gmail.com>
2014-08-19 08:19:40 -0400

    Add start script

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,5 @@ var tm = require('task-master');
 module.exports = function(grunt) {
   tm(grunt);
   grunt.registerTask('default', ['clean', 'symlink', 'concat:dev', 'less:dev', 'markdown', 'server', 'watch']);
-  grunt.registerTask('heroku:production', ['clean', 'symlink', 'concat:dist', 'uglify:dist', 'less:dist', 'markdown', 'server']);
+  grunt.registerTask('heroku:production', ['clean', 'symlink', 'concat:dist', 'uglify:dist', 'less:dist', 'markdown']);
 };

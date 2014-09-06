e0d0c4540e137af267add166f83eea0c1bef5f08
Andrew Nichols <tandrewnichols@gmail.com>
2014-09-06 11:57:07 -0400

    Refactor app.js

--- app.js
+++ app.js
@@ -4,21 +4,11 @@ var express = require('express');
 var app = express();
 var swig = require('swig');
 var fs = require('fs');
-var fm = require('file-manifest');
+var loader = require('./lib/mod-loader');
 var _ = require('lodash');
 var extend = require('config-extend');
-var nconf = require('nconf')
-  .argv()
-  .env()
-  .file({ file: './config/' + (process.env.NODE_ENV || 'development') + '.json' });
-
-var modules = fm.generate(__dirname + '/views/modules', function(memo, file) {
-  memo.me = memo.me || [];
-  memo.mantacode = memo.mantacode || [];
-  if (~file.indexOf('tandrewnichols')) memo.me.push(path.basename(file, '.html'));
-  else if (~file.indexOf('mantacode')) memo.mantacode.push(path.basename(file, '.html'));
-  return memo;
-}, []);
+var nconf = require('nconf').argv().env().file({ file: './config/' + (process.env.NODE_ENV || 'development') + '.json' });
+var modules = loader({ mantacode: [], tandrewnichols: [] }).load('tandrewnichols').load('mantacode').val();
 
 app.set('port', nconf.get('PORT'));
 app.engine('html', require('swig').renderFile);
@@ -43,7 +33,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/modules/:module', function(req, res, next) {
-  var author = _.contains(modules.me, req.params.module) ? 'tandrewnichols' : 'mantacode';
+  var author = _.contains(modules.tandrewnichols, req.params.module) ? 'tandrewnichols' : 'mantacode';
 
   res.render('modules/' + author + '/' + req.params.module, {
     moduleName: req.params.module,
--- tasks/watch.js
+++ tasks/watch.js
@@ -4,7 +4,7 @@ module.exports = {
     tasks: ['less:dev', 'server']
   },
   server: {
-    files: ['app.js'],
+    files: ['app.js', 'lib/**/*.js'],
     tasks: ['server']
   },
   templates: {

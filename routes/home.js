e34244f33c68ef2018b5c2d312e37ee750cd7059
Andrew Nichols <tandrewnichols@gmail.com>
2014-12-12 08:06:52 -0500

    Remove hideNav from home

--- routes/home.js
+++ routes/home.js
@@ -2,5 +2,5 @@ var router = module.exports = require('express').Router();
 var nconf = require('nconf');
 
 router.get('/', function(req, res) {
-  res.render('home', { hideNav: nconf.get('env') === 'development' });
+  res.render('home');
 });

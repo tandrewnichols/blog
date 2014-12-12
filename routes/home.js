48fef7d057bad09de84de786ba67c647e3e64d31
Andrew Nichols <tandrewnichols@gmail.com>
2014-12-12 08:09:29 -0500

    Oops, turn ON hideNav on home page

--- routes/home.js
+++ routes/home.js
@@ -2,5 +2,5 @@ var router = module.exports = require('express').Router();
 var nconf = require('nconf');
 
 router.get('/', function(req, res) {
-  res.render('home');
+  res.render('home', { hideNav: true });
 });

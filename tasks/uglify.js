f67495aa598704e883885dd197945837de9a87ec
Andrew Nichols <tandrewnichols@gmail.com>
2014-12-12 07:53:19 -0500

    Turn off compression?

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,9 +1,7 @@
 module.exports = {
   dist: {
     options: {
-      sourceMap: true,
-      sourceMapName: 'generated/app.js.map',
-      compress: true
+      sourceMap: true
     },
     src: 'generated/app.js',
     dest: 'public/app.js'
